ae7ef39df838087edc6cfead01ccf3553a8b56f9
PAWAN CHAUHAN <167066908+pawan-kumar-chauhan-343@users.noreply.github.com>
2024-06-28 11:39:30 +0530

    Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,14 @@ import "./Home.css";
 const Home = () => {
   return (
     <>
-      <div className="card">
-        <div className="card-body">
-          <p className="card-text">
+    <div className="card_Mh">
+      <div className="card_h">
+        
             Bonstay always provides you an amazing pleasant stay with your
             friends and family at reasonable prices.We provide well-designed
             space with modern amenities.You can reserve a roomfaster with our
             efficient Bonstay app.
-          </p>
-        </div>
+      </div>
       </div>
     </>
   );
